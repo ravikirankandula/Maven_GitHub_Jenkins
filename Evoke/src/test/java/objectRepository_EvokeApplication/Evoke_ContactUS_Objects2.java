3367b8160b30a75f50d9deab589e1cd22f991820
rkandula <rkandula@RKANDULA-L-1071.evoketechnologies.com>
2018-09-15 15:41:25 +0530

    commit

---
+++ Evoke/src/test/java/objectRepository_EvokeApplication/Evoke_ContactUS_Objects2.java
@@ -0,0 +1,23 @@
+package objectRepository_EvokeApplication;
+
+import org.openqa.selenium.By;
+
+public class Evoke_ContactUS_Objects2 {
+
+	public static final By contactus=By.partialLinkText("Contact Us");
+	
+	public static final By firstname=By.name("firstname");
+	
+	public static final By lastname=By.name("lastname");
+	
+	public static final By email=By.name("email");
+	
+	public static final By phone=By.name("phone");
+	
+	public static final By how_can_we_help_=By.name("how_can_we_help_");
+
+	public static final By submit=By.xpath("//*[@value=\"Submit\"]");
+	
+	
+	
+}
